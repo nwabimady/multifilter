f5cf971eba647393aaaee431584ff79386383dbe
nwabimady <nwabimady@gmail.com>
2024-01-09 21:52:02 +0000

    Responsive arrows

--- components/ScrollableMenu.tsx
+++ components/ScrollableMenu.tsx
@@ -1,25 +1,40 @@
 "use client"
 
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Arrow from './Arrow';
 
 const ScrollableMenu: React.FC = ({ children }) => {
-    const menuRef = useRef(null);
-  
-    const scroll = (direction) => {
-      const childWidth = menuRef.current.firstChild.offsetWidth;
-      menuRef.current.scrollLeft += childWidth * direction;
+  const menuRef = useRef(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      const { scrollWidth, clientWidth } = menuRef.current;
+      setIsOverflowing(scrollWidth > clientWidth);
     };
-  
-    return (
-      <div className="scrollable-menu flex items-center">
-        <Arrow direction="left" onClick={() => scroll(-1)} />
-        <ul ref={menuRef} className="menu flex overflow-x-auto scrollbar-hide">
-          {children}
-        </ul>
-        <Arrow direction="right" onClick={() => scroll(1)} />
-      </div>
-    );
+
+    checkOverflow();
+    window.addEventListener('resize', checkOverflow);
+
+    return () => {
+      window.removeEventListener('resize', checkOverflow);
+    };
+  }, [children]); // Re-run the effect if children changes
+
+  const scroll = (direction) => {
+    const childWidth = menuRef.current.firstChild.offsetWidth;
+    menuRef.current.scrollLeft += childWidth * direction;
   };
+
+  return (
+    <div className="scrollable-menu flex items-center">
+      {isOverflowing && <Arrow direction="left" onClick={() => scroll(-1)} />}
+      <ul ref={menuRef} className="menu flex overflow-x-auto scrollbar-hide">
+        {children}
+      </ul>
+      {isOverflowing && <Arrow direction="right" onClick={() => scroll(1)} />}
+    </div>
+  );
+};
 
 export default ScrollableMenu;
